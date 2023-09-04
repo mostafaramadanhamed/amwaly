5e7a0f04f00d1b04a983437615760511f37574a7
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2023-09-04 10:25:16 +0300

    add utils

---
+++ lib/core/utils/utils.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+abstract class Utils{
+
+  void push(context,Widget screen){
+    Navigator.of(context).push(MaterialPageRoute(builder: (context)=>screen,));
+  }
+  void pop(context){
+    Navigator.of(context).pop();
+  }
+
+}
+extension EmptyPadding on num{
+  SizedBox get ph=> SizedBox(
+      height: toDouble(),
+      ); SizedBox get pw=> SizedBox(
+      width: toDouble(),
+      );
+}
