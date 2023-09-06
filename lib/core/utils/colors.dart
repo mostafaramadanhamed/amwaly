1ca6f0ff5711f3baadd118afa9c1204b3091ba8e
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2023-09-06 11:49:14 +0300

    add app colors

--- lib/core/utils/colors.dart
+++ lib/core/utils/colors.dart
@@ -1,3 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
 abstract class AppColors{
 
+  static const kWhiteColor=Color(0xffFFFFFF);
+  static const kPrimaryColor=Color(0xFF222831);
+  static const kSecondaryColor=Color(0xFF393E46);
+  static const kTextColor=Color(0xFF00ADB5);
+  static const kLightColor=Color(0xFFEEEEEE);
 }
--- lib/presentation/views/home_view.dart
+++ lib/presentation/views/home_view.dart
@@ -6,7 +6,6 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
     );
   }
 }
