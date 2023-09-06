9efd8ae257c9f629ab59da273c637769bf59fed6
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2023-09-06 11:55:29 +0300

    add app styles

--- lib/core/utils/colors.dart
+++ lib/core/utils/colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 abstract class AppColors{
 
   static const kWhiteColor=Color(0xffFFFFFF);
+  static const kHintColor=Color(0xffB0B0B0);
   static const kPrimaryColor=Color(0xFF222831);
   static const kSecondaryColor=Color(0xFF393E46);
   static const kTextColor=Color(0xFF00ADB5);
--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,4 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/core/utils/colors.dart';
+
 abstract class AppStyles{
 
-
+static const textStyle22=TextStyle(
+  fontSize: 22,
+  fontWeight: FontWeight.bold,
+  color: AppColors.kPrimaryColor,
+);static const textStyle20=TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w800,
+  color: AppColors.kTextColor,
+);static const textStyle18=TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w800,
+  color: AppColors.kSecondaryColor,
+);static const textStyle17=TextStyle(
+  fontSize: 17,
+  fontWeight: FontWeight.bold,
+  color: AppColors.kPrimaryColor,
+);static const textStyle15=TextStyle(
+  fontSize: 15,
+  fontWeight: FontWeight.bold,
+  color: AppColors.kHintColor,
+);
 }
