a3ccfee72dce9703344bc055e6540009dcd0af9b
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2023-09-07 10:27:49 +0300

    add custom text field

---
+++ lib/core/widgets/text_field.dart
@@ -0,0 +1,38 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:untitled/core/utils/styles.dart';
+
+import '../utils/colors.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({Key? key, this.controller, required this.hint}) : super(key: key);
+ final TextEditingController? controller;
+ final String hint;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      child: TextField(
+        controller: controller,
+        keyboardType: TextInputType.text,
+        // textDirection: TextDirection.ltr,
+        textInputAction: TextInputAction.done,
+        cursorColor: AppColors.kLightColor,
+        style: AppStyles.textStyle17,
+        decoration: InputDecoration(
+          hintText: hint.tr(),
+          hintStyle: AppStyles.textStyle15,
+          border: InputBorder.none,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: const BorderSide(color: AppColors.kPrimaryColor),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: const BorderSide(color: AppColors.kPrimaryColor),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/presentation/views/home_view.dart
+++ lib/presentation/views/home_view.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/core/utils/utils.dart';
 import 'package:untitled/core/widgets/button.dart';
 import 'package:untitled/core/widgets/custom_app_bar.dart';
+import 'package:untitled/core/widgets/text_field.dart';
 
-class HomeView extends StatelessWidget {
-  const HomeView({Key? key}) : super(key: key);
+import '../../core/utils/colors.dart';
+
+class HomeView extends StatefulWidget {
+   const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+TextEditingController phoneController=TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +24,9 @@ class HomeView extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
         child: Column(
           children: [
-            ButtonWidget(onTap: (){},)
+            ButtonWidget(onTap: (){},),
+            20.ph,
+            CustomTextField(hint: 'add')
           ],
         ),
       ),
